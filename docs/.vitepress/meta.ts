9402ad5dbc11f87d9c7b65cb94342c416c3a3610
Younho Choo <yo+github@younho9.com>
2022-07-26 23:50:37 +0900

    chore: update meta data

---
+++ docs/.vitepress/meta.ts
@@ -0,0 +1,29 @@
+/* Texts */
+export const title = 'Younho9 Note';
+export const description =
+	'새로 알게 된 것과 생각을 기록하는 개인적인 노트입니다. 완벽이 아니라 시간이 지남에 따라 조금씩 자라는 것을 목표로 하는 디지털 정원입니다.';
+export const author = 'Younho Choo';
+
+/* Head */
+export const ogUrl = 'https://notes.younho9.com';
+export const ogImage = `${ogUrl}/og-image.png`;
+export const keywords = [
+	'digital-garden',
+	'second-brain',
+	'notes',
+	'journals',
+	'wiki',
+	'personal-wiki',
+	'document',
+];
+
+/* Social Links */
+export const github = 'https://github.com/younho9';
+export const twitter = 'https://twitter.com/younho_9';
+export const linkedin = 'https://www.linkedin.com/in/younho9';
+export const facebook = 'https://www.facebook.com/younho9.choo';
+export const instagram = 'https://www.instagram.com/younho_9/';
+
+/* Images */
+export const logo =
+	'https://raw.githubusercontent.com/younho9/younho9.dev/main/src/assets/logo.png';
