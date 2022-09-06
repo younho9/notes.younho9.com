c8d266fa1d1383f63f46a3b7e7a14944470aab1d
Younho Choo <yo+github@younho9.com>
2022-09-06 17:03:02 +0900

    feat: add random nav button (#30)

---
+++ docs/.vitepress/theme/components/Random.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {onMounted} from 'vue';
+import pluralize from 'pluralize';
+import data from '../../../data.json';
+import {values, random} from 'lodash-es';
+import {useRouter} from 'vitepress';
+
+const router = useRouter();
+
+const color = getComputedStyle(document.documentElement).getPropertyValue(
+	'--vp-c-brand',
+);
+const loaderOptions = {
+	loading: true,
+	style: {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+	},
+	size: '10px',
+	color,
+};
+
+onMounted(() => {
+	const files = values(data).flat();
+	const randomNumber = random(0, files.length - 1);
+	const randomFile = files[randomNumber];
+	const randomURL = new URL(
+		`/${pluralize(randomFile.type)}/${randomFile.fileName}.html`,
+		window.location.origin,
+	);
+
+	router.go(randomURL.href);
+});
+</script>
+
+<template>
+	<div style="position: fixed; top: 0; left: 0; right: 0; bottom: 0">
+		<GridLoader v-bind="loaderOptions" />
+	</div>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,9 @@
 import Theme from 'vitepress/theme';
+import GridLoader from 'vue-spinner/src/GridLoader.vue';
 import DocIndex from './components/DocIndex.vue';
 import Graph from './components/Graph.vue';
 import LatestDocs from './components/LatestDocs.vue';
+import Random from './components/Random.vue';
 
 import './style/vars.css';
 import './style/main.css';
@@ -12,5 +14,7 @@ export default {
 		app.component('DocIndex', DocIndex);
 		app.component('Graph', Graph);
 		app.component('LatestDocs', LatestDocs);
+		app.component('Random', Random);
+		app.component('GridLoader', GridLoader);
 	},
 };
