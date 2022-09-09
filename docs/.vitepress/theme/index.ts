3b75b714461a21eba9f602d67037d069917450fd
Younho Choo <yo+github@younho9.com>
2022-09-09 13:27:18 +0900

    feat: update metadata table

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,16 +1,18 @@
-import Theme from 'vitepress/theme';
+import {Theme} from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
 import GridLoader from 'vue-spinner/src/GridLoader.vue';
 import DocIndex from './components/DocIndex.vue';
 import Graph from './components/Graph.vue';
 import LatestDocs from './components/LatestDocs.vue';
 import Metadata from './components/Metadata.vue';
 import Random from './components/Random.vue';
+import Tags from './components/Tags.vue';
 
 import './style/vars.css';
 import './style/main.css';
 
-export default {
-	...Theme,
+export default <Theme>{
+	...DefaultTheme,
 	enhanceApp({app}) {
 		app.component('GridLoader', GridLoader);
 		app.component('DocIndex', DocIndex);
@@ -18,5 +20,6 @@ export default {
 		app.component('LatestDocs', LatestDocs);
 		app.component('Metadata', Metadata);
 		app.component('Random', Random);
+		app.component('Tags', Tags);
 	},
 };
